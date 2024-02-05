c5be376df65872d19f719d0cc113a72ffe44257f
blackshirt <blackshirt@users.noreply.github.com>
2024-02-05 19:46:28 +0700

    Update custom.v

--- custom.v
+++ custom.v
@@ -2,43 +2,46 @@ module poly1305
 
 import math.bits
 
-// custom allocators that align with existing builtin structures
-// Internally it currently uses the Uint256 structure as a custom allocator
-type Acc = unsigned.Uint256
-
-// mul_64 returns u*v even the result size is over > 256 bit
+// Uint192 is a custom allocators that represents 192 bits of unsigned integer.
+// Maybe this structure could supplement `math.unsigned` module, but it is another story.
+struct Uint192 {
+	lo u64
+	mi u64
+	hi u64
+}
+	
+// We define several required functionality on this custom allocator.
+//
+// mul_64_checked returns u*v even the result size is over > 256 bit
 // its acts like Uint128.overflowing_mul_64 but return value instead of boolean.
-fn (u Acc) mul_64_checked(v u64) (Acc, u64) {
-	//           u.hi	      u.lo
+fn (u Uint192) mul_64_checked(v u64) (Uint192, u64) {
+	//         u.hi	  u.mi   u.lo
 	//							v
 	// --------------------------------- x
-	// 				m1			m0 					// 128 bit product
+	// 	  m2		  m1		  m0 				// 128 bit product
 	// -----------------------------------
-	// 	 m3.hi	  m2.hi			
-	//			  m3.lo				m0.lo
+	// 	  m2.hi       m1.hi       m0.hi			
+	//		          m2.lo       m1.lo    m0.lo
 	// ------------------------------------------- +
-	//	  t4	  t3		 t2		 t1		  t0
-	m0 := u128_mul(u.lo.lo, v)
-	m1 := u128_mul(u.lo.hi, v)
-	m2 := u128_mul(u.hi.lo, v)
-	m3 := u128_mul(u.hi.hi, v)
+	//	    t3	         t2		    t1		 t0	 
+	// 
+	m0 := u128_mul(u.lo, v)
+	m1 := u128_mul(u.mi, v)
+	m2 := u128_mul(u.hi, v)
 	//
 	t0, c0 := bits.add_64(m0.lo, 0, 0)
 	t1, c1 := bits.add_64(m0.hi, m1.lo, c0)
 	t2, c2 := bits.add_64(m1.hi, m2.lo, c1)
-	t3, c3 := bits.add_64(m2.hi, m3.lo, c2)
-	t4, c4 := bits.add_64(m3.hi, 0, c3)
-	if c4 != 0 {
+	t3, c3 := bits.add_64(m2.hi, 0, c2)
+	if c3 != 0 {
 		panic('Custom Acc overflow')
 	}
-	x := unsigned.Uint256
-	{
-		lo:
-		unsigned.uint128_new(t0, t1)
-		hi:
-		unsigned.uint128_new(t2, t3)
+	x := unsigned.Uint192{
+		lo: t0
+		mi: t1
+		hi: t2
 	}
-	return Acc(x), t4
+	return x, t4
 }
 
 fn (u Acc) mul_128_checked(v unsigned.Uint128) (Acc, unsigned.Uint128) {
