0221c666c5871bb3736de29d12b9268a193a8d46
blackshirt <blackshirt@users.noreply.github.com>
2024-02-05 20:57:37 +0700

    Update custom.v

--- custom.v
+++ custom.v
@@ -12,36 +12,39 @@ struct Uint192 {
 	
 // We define several required functionality on this custom allocator.
 //
-// mul_64_checked returns u*v even the result size is over > 256 bit
-// its acts like Uint128.overflowing_mul_64 but return value instead of boolean.
+// mul_64_checked returns u*v even the result size is over > 192 bit.
+// It return (Uin192, u64) pair where the former stores low 192 bit and 
+// and the rest of high bit stored in the u64 part. You can check the value of u64 part
+// for v != 0, its mean, the product of u*v is overflowing 192 bits.
 fn (u Uint192) mul_64_checked(v u64) (Uint192, u64) {
 	//         u.hi	  u.mi   u.lo
 	//							v
 	// --------------------------------- x
-	// 	  m2		  m1		  m0 				// 128 bit product
+	// 	        m2	    m1	   m0 				// 128 bit product
 	// -----------------------------------
-	// 	  m2.hi       m1.hi       m0.hi			
-	//		          m2.lo       m1.lo    m0.lo
+	// 	     m2.hi    m1.hi   m0.hi			
+	//		          m2.lo   m1.lo    m0.lo
 	// ------------------------------------------- +
-	//	    t3	         t2		    t1		 t0	 
+	//	       t3	     t2		 t1		 t0	 
 	// 
 	m0 := u128_mul(u.lo, v)
 	m1 := u128_mul(u.mi, v)
 	m2 := u128_mul(u.hi, v)
-	//
+	// propagates carry
 	t0, c0 := bits.add_64(m0.lo, 0, 0)
 	t1, c1 := bits.add_64(m0.hi, m1.lo, c0)
 	t2, c2 := bits.add_64(m1.hi, m2.lo, c1)
 	t3, c3 := bits.add_64(m2.hi, 0, c2)
+	// something bad happen if the last c3 is non null
 	if c3 != 0 {
 		panic('Custom Acc overflow')
 	}
-	x := unsigned.Uint192{
+	x := Uint192{
 		lo: t0
 		mi: t1
 		hi: t2
 	}
-	return x, t4
+	return x, t3
 }
 
 fn (u Acc) mul_128_checked(v unsigned.Uint128) (Acc, unsigned.Uint128) {
